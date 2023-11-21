3ae805d8af128d5e977b196ec65b0df6f4679fc6
mahiiram <mahendran.ramar9797@gmail.com>
2023-11-22 02:57:50 +0530

    new commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,15 +7,7 @@ import { useNavigate } from 'react-router-dom';
 function Home() {
   const navigate = useNavigate();
   useEffect(() => {
-    const unlisten = navigate(location => {
-      // Check if the user navigated to the homepage
-      if (location.pathname === '/') {
-        console.log('Navigated to the homepage');
-      }
-    });
-    return () => {
-      unlisten();
-    };
+      navigate('/')
   }, [navigate]);
 
   return (
